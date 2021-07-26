82fd62827d7775aafa6d8a4038fa524fdd23d5ee
Chandan Rai <chank.raiii@gmail.com>
2021-07-26 12:06:44 +0530

    player input is done

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import "./App.css";
+import { useState } from "react";
 
 import GameGreeting from "./components/GameGreeting";
 import PlayerName from "./components/PlayerName";
 
 function App() {
+  const [playerName, setPlayerName] = useState({
+    player1: "",
+    player2: "",
+  });
+
   return (
     <div className="wrapper">
       <div className="game-greeting">
         <GameGreeting />
       </div>
       <div className="player-names">
-        <PlayerName />
+        <PlayerName playerName={playerName} setPlayerName={setPlayerName} />
       </div>
     </div>
   );
--- src/components/PlayerName.js
+++ src/components/PlayerName.js
@@ -1,36 +1,48 @@
 import "./style/PlayerName.css";
+// import { useState } from "react";
 
-export default function PlayerName() {
+export default function PlayerName(props) {
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    props.setPlayerName({
+      ...props.playerName,
+      [name]: value,
+    });
+  };
   return (
-    <>
-      <form>
-        <fieldset>
-          <legend>Enter Player Name</legend>
-          <div className="form-floating mb-3">
-            <input
-              name="player1"
-              id="player1-input"
-              className="form-control player-1"
-              placeholder="X"
-            />
-            <label htmlFor="player1-input">Player X</label>
-          </div>
+    <form>
+      <fieldset>
+        <legend>Enter Player Name</legend>
+        {/* <p style={{ color: "white" }}>{props.playerName.player1}</p>
+        <p style={{ color: "white" }}>{props.playerName.player2}</p> */}
+        <div className="form-floating mb-3">
+          <input
+            id="player1-input"
+            name="player1"
+            value={props.playerName.player1}
+            className="form-control player-1"
+            placeholder="X"
+            onChange={handleInputChange}
+          />
+          <label htmlFor="player1-input">Player X</label>
+        </div>
 
-          <div className="form-floating mb-4">
-            <input
-              name="player2"
-              id="player2-input"
-              className="form-control player-2"
-              placeholder="O"
-            />
-            <label htmlFor="player2-input">Player O</label>
-          </div>
+        <div className="form-floating mb-4">
+          <input
+            id="player2-input"
+            name="player2"
+            value={props.playerName.player2}
+            className="form-control player-2"
+            placeholder="O"
+            onChange={handleInputChange}
+          />
+          <label htmlFor="player2-input">Player O</label>
+        </div>
 
-          <div className="start-btn">
-            <button> Start </button>
-          </div>
-        </fieldset>
-      </form>
-    </>
+        <div className="start-btn">
+          <button> Start </button>
+        </div>
+      </fieldset>
+    </form>
   );
 }
